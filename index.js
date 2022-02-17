f7ea72d41c6acd4915741edc241ba4be2c9eedd3
J Alexander Martinez <Jalexmartinez1013@hotmail.com>
2022-02-17 10:57:16 -0500

    framed out api

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express'); //importing express
+const cors = require('cors'); //importing cors
+const {getTasks} = require('./src/task'); //importing get Task
+const PORT = process.env.PORT || 3000; // conecting to port
+
+const app = express();   // intailizing express
+app.use(cors());        //Telling Express to use Cors
+app.use(express.json());  //Telling api to parce info as json
+
+
+//Routes
+// app.post('/tasks', createTask);   
+app.get('/task', getTasks);
+// app.patch('/task/:taskId', updateTask);
+
+
+app.listen(PORT, ()=> {          //connecting to port
+    console.log('Listening on Port:', PORT)
+});
